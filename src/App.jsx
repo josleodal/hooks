3cb8898ba6290b76eafec2c3617b7cecb34823cd
josleodal <josleodal@gmail.com>
2024-04-09 19:09:18 +0200

    subida

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import './App.css';
+import React from 'react';
+import useCustomHook from './hooks/useFecthCharacters';
 
-function App() {
+const App = () => {
   const urlPokemon = 'https://pokeapi.co/api/v2/pokemon/1';
   const urlRick = 'https://rickandmortyapi.com/api/character/1';
-  
+
+  const pokemonData = useCustomHook(urlPokemon);
+  const rickData = useCustomHook(urlRick);
+
   return (
-    <>
-    </>
+    <div>
+      <h2>Personaje Pokemon</h2>
+      {pokemonData && (
+        <>
+          <p>{pokemonData.name}</p>
+          <img src={pokemonData.sprites.front_default} alt="Pokemon" />
+        </>
+      )}
+
+      <h2>Personaje Rick and Morty</h2>
+      {rickData && (
+        <>
+          <p>{rickData.name}</p>
+          <img src={rickData.image} alt="Rick and Morty" />
+        </>
+      )}
+    </div>
   );
-}
+};
 
 export default App;
---
+++ src/hooks/useFecthCharacters.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+
+const useCustomHook = (url) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      
+    };
+  }, [url]);
+
+  return data;
+};
+
+export default useCustomHook;
